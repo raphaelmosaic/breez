d781df2dbafeef09a1af01763a074651a20cf9ee
Raphael <98910367+raphaelmosaic@users.noreply.github.com>
2023-09-23 17:26:23 +0200

    run breez is now a menubar app

--- 3_run_breez.py
+++ 3_run_breez.py
@@ -1,27 +1,33 @@
+import rumps
 import subprocess
 import os
 
-def run_breez():
-    # Dynamically get the home directory
-    home_directory = os.path.expanduser("~")
+# Function to run the Breez script
+def run_breez(sender):
+    home_directory = os.path.expanduser("~")  # Dynamically get the home directory
+    breez_home_folder = os.path.join(home_directory, "breez")  # Append the 'breez' folder to it
     
-    # Append the 'breez' folder to it
-    breez_home_folder = os.path.join(home_directory, "breez")
-    
-    # Full path to main.py (You might want to change this to run a different script)
+    # Full path to main.py
     main_script = os.path.join(breez_home_folder, "main.py")
     
     try:
         print("Breez is running. Press the shortcut you selected to fix the current line in any application.")
-        
-        # Try running the script with Python
-        subprocess.run(["python", main_script])
+        subprocess.run(["python", main_script])  # Try running the script with Python
     except FileNotFoundError:
         try:
-            # If Python failed, try Python3
-            subprocess.run(["python3", main_script])
+            subprocess.run(["python3", main_script])  # If Python failed, try Python3
         except Exception as e:
             print(f"Failed to run Breez: {e}")
 
+# Create the menubar app
+class BreezApp(rumps.App):
+    def __init__(self):
+        super(BreezApp, self).__init__("Breez")
+        self.menu = ["Run Breez"]
+        
+    @rumps.clicked("Run Breez")
+    def on_run(self, sender):
+        run_breez(sender)
+
 if __name__ == "__main__":
-    run_breez()
+    BreezApp().run()
