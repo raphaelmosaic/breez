0513e18fb3e9e2e71385a143f8d9dbd4c3b14090
Raphael <98910367+raphaelmosaic@users.noreply.github.com>
2023-09-23 16:14:04 +0200

    x

--- 3_run_breez.py
+++ 3_run_breez.py
@@ -8,14 +8,17 @@ def run_breez():
     # Append the 'breez' folder to it
     breez_home_folder = os.path.join(home_directory, "breez")
     
-    # Full path to main.py
+    # Full path to main.py (You might want to change this to run a different script)
     main_script = os.path.join(breez_home_folder, "main.py")
-
+    
     try:
         print("Breez is running. Press the shortcut you selected to fix the current line in any application.")
+        
+        # Try running the script with Python
         subprocess.run(["python", main_script])
     except FileNotFoundError:
         try:
+            # If Python failed, try Python3
             subprocess.run(["python3", main_script])
         except Exception as e:
             print(f"Failed to run Breez: {e}")
