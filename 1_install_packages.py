d3348f81c7ac224faef0d7528373572df542479c
Raphael <98910367+raphaelmosaic@users.noreply.github.com>
2023-09-24 20:10:40 +0200

    x

--- 1_install_packages.py
+++ 1_install_packages.py
@@ -2,10 +2,10 @@ import subprocess
 
 def install_packages():
     try:
-        subprocess.run(["pip", "install", "requests", "pynput", "pyperclip, rumps"])
+        subprocess.run(["pip", "install", "requests", "pynput", "pyperclip, rumps, urllib3==1.26.6"])
     except Exception:
         try:
-            subprocess.run(["pip3", "install", "requests", "pynput", "pyperclip"])
+            subprocess.run(["pip3", "install", "requests", "pynput", "pyperclip, rumps, urllib3==1.26.6"])
         except Exception as e:
             print(f"Failed to install packages: {e}")
             return
